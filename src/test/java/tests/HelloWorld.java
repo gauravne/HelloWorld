ce13cfd47842abfdc56bbfe258be56e45d46c23b
gauravne <gauravneware1997@gmail.com>
2026-01-03 01:26:31 +0530

    google changes

--- src/test/java/tests/HelloWorld.java
+++ src/test/java/tests/HelloWorld.java
@@ -1,13 +1,20 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class HelloWorld {
 	
 	@Test
 	public void hello()
 	{
-		
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://www.google.com");
 		System.out.println("Gaurav Neware");
 	}
 
