072524eecbb8c0142c2c041f45eb8befbcf1d0fa
gauravne <gauravneware1997@gmail.com>
2026-01-03 15:02:41 +0530

    added loggin in user for jenkins

--- src/test/java/tests/HelloWorld.java
+++ src/test/java/tests/HelloWorld.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
@@ -12,8 +13,16 @@ public class HelloWorld {
 	@Test
 	public void hello()
 	{
-		WebDriverManager.edgedriver().setup();
-		WebDriver driver = new EdgeDriver();
+		//WebDriverManager.edgedriver().setup();
+		//WebDriver driver = new EdgeDriver();
+		ChromeOptions options = new ChromeOptions();
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver(options);
+
+		options.addArguments("--headless=new");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		
 		driver.get("https://www.google.com");
 		System.out.println("Gaurav Neware");
 	}
