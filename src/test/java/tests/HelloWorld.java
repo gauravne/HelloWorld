8ae7377a1d8ff6740e685974e94fbb5042f5c73c
gauravne <gauravneware1997@gmail.com>
2026-01-03 15:15:19 +0530

    edge changes

--- src/test/java/tests/HelloWorld.java
+++ src/test/java/tests/HelloWorld.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -12,16 +13,25 @@ public class HelloWorld {
 	
 	@Test
 	public void hello()
-	{
-		//WebDriverManager.edgedriver().setup();
+	{	 EdgeOptions options = new EdgeOptions();
+		WebDriverManager.edgedriver().setup();
 		//WebDriver driver = new EdgeDriver();
-		ChromeOptions options = new ChromeOptions();
+	
+		    WebDriver driver = new EdgeDriver(options);
+		    options.addArguments("--headless=new");
+		    options.addArguments("--disable-gpu");
+		    options.addArguments("--window-size=1920,1080");
+
+
+		
+		
+	/*	ChromeOptions options = new ChromeOptions();
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver(options);
 
 		options.addArguments("--headless=new");
 		options.addArguments("--no-sandbox");
-		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-dev-shm-usage");*/
 		
 		driver.get("https://www.google.com");
 		System.out.println("Gaurav Neware");
